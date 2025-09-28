93058a79c475a9ec202c3bd53dd322eb93c2cbce
Prince Boaz <b.iza@alustustudent>
2025-09-28 17:35:24 +0200

    download feature for situation and report pompiste

---
+++ home/downloadReport.js
@@ -0,0 +1,29 @@
+function download() {
+    try {
+        // Ensure data is up to date
+        // If your displayDetails() fetches/fills data, call it here or make sure it's already run
+        // displayDetails();
+
+        // Choose the section to export: body, main, or a wrapper
+
+        const logDate = document.getElementById("logDate").value;
+        // const email = document.getElementById("email").value;
+
+        const element = document.body;
+
+        const opt = {
+        margin:       0.4,
+        filename:     logDate + ".pdf",
+        image:        { type: "jpeg", quality: 0.98 },
+        html2canvas:  { scale: 4, useCORS: true, scrollY: 0 },        
+        jsPDF:        { unit: "px", format: [element.scrollWidth, element.scrollHeight], orientation: "portrait" },
+        pagebreak:    { mode: ['css', 'legacy'] } 
+        };
+
+        html2pdf().set(opt).from(element).save();
+
+    } catch (error) {
+        console.log("This is the error ", error);
+        
+    }
+}
