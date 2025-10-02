5d1953177d2a2373b8011edc5dea108e3f59f5e3
Prince Boaz <b.iza@alustustudent>
2025-10-02 13:39:26 +0200

    added checuser on every webpage

--- home/checkUser.js
+++ home/checkUser.js
@@ -5,14 +5,14 @@
 //     const ip = data.ip;
 
 //         //List of allowed IPs/networks
-//     const allowed = ["41.216.105.56", "105.178.32.18"];
+//     const allowed = ["41.216.105.56", "41.186.132.111"];
 
 //     if (!allowed.includes(ip)) {
-//         document.body.innerHTML = "<h1>🚫 Access Denied</h1>";
+//         document.body.innerHTML = "<h1>🚫 Access Denied</h1>";        
 //     }
 //     } catch (err) {
 //     console.error("Failed to check access", err);
 //     }
-//     }
+// }
 
 // checkAccess();
