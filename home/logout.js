3ad0f2864dcce5594b081235530c3fa2c815cd9f
Prince Boaz <b.iza@alustustudent>
2025-10-03 13:26:20 +0200

    Added a logout pop up

--- home/logout.js
+++ home/logout.js
@@ -14,3 +14,25 @@ async function logout() {
     console.log("error for page access",error);
     }
 }
+
+function logoutBlock() {  
+  try {
+    document.getElementById("logoutBtn").addEventListener("click", function() {
+    document.getElementById("logoutPopup").style.display = "block";
+    });
+
+    document.getElementById("cancelLogout").addEventListener("click", function() {
+    document.getElementById("logoutPopup").style.display = "none";
+    window.location.reload();
+    });
+
+    document.getElementById("confirmLogout").addEventListener("click", function() {
+    logout();
+    alert("Logged out successfully!");
+    document.getElementById("logoutPopup").style.display = "none";
+    });
+  } catch (error) {
+    console.log("This is the error :",error);
+    
+  }
+}
