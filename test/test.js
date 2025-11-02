62ba22f6b088f1463f3a8b995416f841f00924b4
Prince Boaz <b.iza@alustustudent>
2025-11-02 03:48:57 +0200

    today we start

--- test/test.js
+++ test/test.js
@@ -1,9 +1,7 @@
 async function display() {
     
     try {
-        const check = document.getElementById("check").value;
-        console.log("check",check);
-        
+        const check = document.getElementById("check").value;        
 
         const res = await fetch(`https://mywebapp-backend.onrender.com/api/attributes/${check}`);
         const data = await res.json();
@@ -37,11 +35,86 @@ async function display() {
                 tr.appendChild(td);
             }
 
-            body.appendChild(tr);            
-
+            body.appendChild(tr);
         }
+                    
+        const div = document.getElementById("search")
+
+        div.innerHTML = "";
+
+        const searchWith = document.createElement("h2");
+        searchWith.textContent = `Search With`;
+
+        div.appendChild(searchWith);
+
+        const select = document.createElement("select");
+
+        const optionStart = document.createElement("option");        
+        optionStart.textContent = `Search With`
+        select.appendChild(optionStart)
+
+        for (let i = 0; i < attributes.length; i++) {
+            const option = document.createElement("option")
+            option.textContent = `${attributes[i].key}`
+            select.appendChild(option);            
+        }
+
+        div.appendChild(select);
+
+        div.appendChild(document.createElement("br"));
+
+        const input = document.createElement("input");  
+        input.type = "text"; 
+        input.id = "searchValue"   
+        input.textContent = ``
+        div.appendChild(input);
+
+        div.appendChild(document.createElement("br"));
+        
+        const submit = document.createElement("button");
+        submit.type = "button"; 
+        submit.className = "action-btn";
+        submit.textContent = "Search";
+        submit.onclick = search();
+        div.appendChild(submit);
+
+        div.appendChild(document.createElement("br"));
+        div.appendChild(document.createElement("br"));
         
     } catch (error) {
         console.log("Error at the display gain:",error);        
+    }
+}
+
+async function search() {
+    try {
+
+        const check = document.getElementById("check").value;        
+
+        const res = await fetch(`https://mywebapp-backend.onrender.com/api/attributes/${check}`);
+        const data = await res.json();
+        const attributes = data.attributes;
+
+        const resDocs = await fetch(`https://mywebapp-backend.onrender.com/api/documents/${check}`);
+        const docData = await resDocs.json();
+        const rows = docData.documents;
+
+        let filteredRows = [];
+
+        const searchWith = document.getElementById("searchWith").value;
+        const searchValue = document.getElementById("searchValue").value;
+
+        for (let i = 0; i < rows.length; i++) {
+        const row = rows[i];
+
+            if (row[searchWith] === searchValue) {
+                filteredRows.push(row);
+            }
+        }
+
+        
+    } catch (error) {
+        console.log(error);
+        
     }
 }
